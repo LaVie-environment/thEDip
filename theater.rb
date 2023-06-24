9600ba8ad1f93031873dcadffedf4b2d86f08830
ziggy <seun_tbello@proton.me>
2023-06-24 14:42:41 +0000

    First attempt to compare the output of the code with DEBUG_MODULE_1 set to true

--- theater.rb
+++ theater.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+# Flag for debugging (change to false when finished debugging)
+DEBUG_MODULE_1 = true
 
 puts "Enter the customer's age: "
 
@@ -7,7 +9,25 @@ puts "Enter the customer's age: "
 age = gets.to_i
 
 # Determine the cost based on age
+if DEBUG_MODULE_1
+  puts age
+end
 
+case
+# '=' is assignment NOT equality test '=='
+when (age = 12) then
+  cost = 9
+when (age >= 65) then
+  cost = 12
+else
+  cost = 18
+end
+if DEBUG_MODULE_1
+  puts age
+end
+
+
+# Determine the cost based on age
 """
 if ((age <= 12) or (age >= 65))
   cost = 9
@@ -18,15 +38,18 @@ end
 """
 
 # Case Statement
-
+"""
 case
-when (age = 12)
+when (age = 12) then
   cost = 9
-when (age >= 65)
+when (age >= 65) then
   cost = 12
 else
   cost = 18
 end
+# DEBUG
+puts age
+"""
 
 # Print out the final cost
 puts "Ticket cost: " + cost.to_s
