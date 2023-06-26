718a9951e42334f26d0819fbd33a125d3be4050b
armzig <bellotaophyc@outlook.com>
2023-06-26 20:29:02 +0100

    To calculate the effect of the difference in wheel

--- klassgear.rb
+++ klassgear.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 class Gear
-    attr_reader :chainring, :cog
+    attr_reader :chainring, :cog :rim, :tire
     def initialize(chainring, cog)
         @chainring = chainring
         @cog = cog
@@ -10,7 +10,11 @@ class Gear
     def ratio
         chainring / cog.to_f
     end
+
+    def gear_inches
+        # tire goes around rim twice for diameter
+        ratio * (rim + (tire * 2))
 end
 
-puts Gear.new(52, 11).ratio
-puts Gear.new(30, 27).ratio
+puts Gear.new(52, 11, 26, 1.5).gear_inches
+puts Gear.new(30, 27, 24, 1.25).gear_inches
