625d7edbb52870cce56e840e80bb8f8c96d990fe
ziggy <seun_tbello@proton.me>
2023-06-27 12:16:40 +0000

    Convert the Wheel Struct to an independent Wheel class and add the new circumference method

--- klassgear.rb
+++ klassgear.rb
@@ -9,10 +9,10 @@ It calculates the gear ratio and gear inches based on the provided chainring, co
 
 class Gear
     attr_reader :chainring, :cog, :wheel
-    def initialize(chainring, cog, rim, tire)
+    def initialize(chainring, cog, wheel=nil)
         @chainring = chainring
         @cog = cog
-        @wheel = Wheel.new(rim, tire)
+        @wheel = wheel
     end
 
     def ratio
@@ -23,13 +23,35 @@ class Gear
         # tire goes around rim twice for diameter
       ratio * wheel.diameter
     end
-
-    Wheel = Struct.new(:rim, :tire) do
-      def diameter
-        rim + (tire * 2)
-      end
-    end
 end
+
+Class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+
+end
+
+@wheel = Wheel.new(26, 1.5)
+puts @wheel.circumference
+
+puts Gear.new(52, 11, @wheel).gear_inches
+
+puts Gear.new(52, 11).ratio
+
+  
+      
 
 
 
